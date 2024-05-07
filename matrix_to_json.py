8b9fc0170adc69b3a9c2360dae1790923fa9391c
ZiruiYin <zirui.yin@duke.edu>
2024-05-07 19:14:33 -0400

    test matrix to json

--- matrix_to_json.py
+++ matrix_to_json.py
@@ -36,7 +36,7 @@ def convert_to_format(depths, global_to_depths):
     return depths_format
 
 
-def process_raw(filepath):  # I cannot find the json_to_cluster_refactor.py so I just put it here. Almost the same code for processing raw json.
+def process_raw(filepath):
     with open(filepath, "r") as file:
         data_dict = json.load(file)
         t = data_dict["trebleNotes"]
@@ -44,39 +44,23 @@ def process_raw(filepath):  # I cannot find the json_to_cluster_refactor.py so I
         ti = data_dict["innerTrebleNotes"]
         bi = data_dict["innerBassNotes"]
         idx = 0
-        t_depth = []
-        b_depth = []
-        ti_depth = []
-        bi_depth = []
         global_to_depths = {}
         for i in range(len(t["pitchNames"])):
             if t["pitchNames"][i] != "_":
-                t_depth.append(t["depths"][i])
                 global_to_depths[idx] = (0, i)
                 idx += 1
-            else:
-                t_depth.append(-1)
             if b["pitchNames"][i] != "_" and b["pitchNames"][i] != t["pitchNames"][i]:
-                b_depth.append(b["depths"][i])
                 global_to_depths[idx] = (3, i)
                 idx += 1
-            else:
-                b_depth.append(-1)
             if ti["pitchNames"][i] != "_" and ti["pitchNames"][i] != t["pitchNames"][i] and ti["pitchNames"][i] != \
                     b["pitchNames"][i]:
-                ti_depth.append(ti["depths"][i])
                 global_to_depths[idx] = (1, i)
                 idx += 1
-            else:
-                ti_depth.append(-1)
             if bi["pitchNames"][i] != "_" and bi["pitchNames"][i] != t["pitchNames"] and bi["pitchNames"][i] != \
                     b["pitchNames"][i] and bi["pitchNames"][i] != ti["pitchNames"][i]:
-                bi_depth.append(bi["depths"][i])
                 global_to_depths[idx] = (2, i)
                 idx += 1
-            else:
-                bi_depth.append(-1)
-    return t_depth, b_depth, ti_depth, bi_depth, global_to_depths
+    return global_to_depths
 
 
 def generate_paths(piece):
@@ -86,20 +70,50 @@ def generate_paths(piece):
     return ground_truth_path, file_path
 
 
-if __name__ == "__main__":
-    piece = 'Primi_1'
+def update_json_depths(ground_truth_path, depths_format):
+    with open(ground_truth_path, 'r') as file:
+        data_dict = json.load(file)
+    
+    data_dict["trebleNotes"]["depths"] = depths_format[0]
+    data_dict["innerTrebleNotes"]["depths"] = depths_format[1]
+    data_dict["innerBassNotes"]["depths"] = depths_format[2]
+    data_dict["bassNotes"]["depths"] = depths_format[3]
+    
+    new_path = ground_truth_path.replace('.json', '_predict.json')
+    with open(new_path, 'w') as file:
+        json.dump(data_dict, file, indent=4)
+    return
+
+
+def matrix_to_json(piece):
     ground_truth_path, file_path = generate_paths(piece)
-    t_depth, b_depth, ti_depth, bi_depth, global_to_positions = process_raw(ground_truth_path)
-    print('\n')
-    print("--------------------Ground Truth--------------------")
-    print(t_depth)
-    print(ti_depth)
-    print(bi_depth)
-    print(b_depth)
-    print('\n')
-    print("--------------------Generated-----------------------")
+    global_to_positions = process_raw(ground_truth_path)
     matrices = read_file(file_path)
     depths = update_depths(matrices)
     depths_format = convert_to_format(depths, global_to_positions)
-    for i in range(len(depths_format)):
-        print(depths_format[i])
+    update_json_depths(ground_truth_path, depths_format)
+    return
+
+
+if __name__ == "__main__":
+    piece = 'Primi_1'
+    matrix_to_json(piece)
+
+
+    #ground_truth_path, file_path = generate_paths(piece)
+    #global_to_positions = process_raw(ground_truth_path)
+    #t_depth, b_depth, ti_depth, bi_depth, global_to_positions = process_raw(ground_truth_path)
+    #print('\n')
+    #print("--------------------Ground Truth--------------------")
+    #print(t_depth)
+    #print(ti_depth)
+    #print(bi_depth)
+    #print(b_depth)
+    #print('\n')
+    #print("--------------------Generated-----------------------")
+    #matrices = read_file(file_path)
+    #depths = update_depths(matrices)
+    #depths_format = convert_to_format(depths, global_to_positions)
+    #new_json_path = update_json_depths(ground_truth_path, depths_format)
+    #for i in range(len(depths_format)):
+    #    print(depths_format[i])
